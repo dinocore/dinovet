3fb1937a2a14d7c00dcd5ce2fac0f85eb8be5518
Chad Jablonski <chad@dinocore.net>
2010-08-28 22:11:58 -0700

    Rspec installed

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,9 @@ module Dinovet
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
