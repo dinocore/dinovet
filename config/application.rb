f8886aa532030b0da73f4e1c3def765946199efe
Chad Jablonski <chad@dinocore.net>
2010-08-28 23:12:45 -0700

    Haml set as default template engine

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,7 @@ module Dinovet
     config.filter_parameters += [:password]
 
     config.generators do |g|
+      g.template_engine :haml
       g.orm :mongo_mapper
       g.test_framework :rspec
     end
