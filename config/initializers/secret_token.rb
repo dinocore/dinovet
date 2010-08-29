65063d9ac1849e57a9dad2764c77d5098b3395c0
Chad Jablonski <chad@dinocore.net>
2010-08-28 22:02:59 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinovet::Application.config.secret_token = 'fb6069eee16008a46d260407edca3902032c471081b35f85cecebfcc56faed75ad4bbfc2a9cec5c2089188176f2b93dcbaa80647e0d22c120adac770dbccf3db'
