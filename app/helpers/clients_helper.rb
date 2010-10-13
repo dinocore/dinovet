2fa82d02ad5bab35f82acfb88c3719f92750ad6a
Chad Jablonski <chad@dinocore.net>
2010-10-13 15:31:44 -0700

    Styled new client form

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,8 @@
 module ClientsHelper
+  def name_last_first(client)
+    header = client.last_name
+    unless client.first_name.blank?
+      header += ", #{client.first_name}"
+    end
+  end
 end
