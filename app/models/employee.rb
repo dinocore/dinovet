aad164c0a376fe49c686d4caaed98095a4196c21
Brandon <blhall@debian-vm.(none)>
2011-01-25 15:09:03 -0500

    Manage_Employee initial commit

---
+++ app/models/employee.rb
@@ -0,0 +1,37 @@
+class Employee
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :title
+  field :first_name
+  field :last_name
+  field :address_1
+  field :address_2
+  field :city
+  field :state
+  field :zipcode
+  field :email
+
+  embeds_many :phone_numbers
+  references_many :patients
+
+  accepts_nested_attributes_for :phone_numbers
+
+  validates_associated :phone_numbers
+  validates_presence_of :first_name, :last_name, :address_1, :city, :state,
+                        :zipcode, :phone_numbers
+  validates_format_of :email, :allow_nil => true,
+                      :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+
+  def self.search(query)
+    
+    if query.blank?
+      return find :all
+    end
+
+    any_of({ :first_name             => /.*#{query}.*/ },
+           { :last_name              => /.*#{query}.*/ },
+           { :address_1              => /.*#{query}.*/ },
+           { :email                  => /.*#{query}.*/ },
+           { "phone_numbers.number"  => /.*#{query}.*/ })
+  end
+end
