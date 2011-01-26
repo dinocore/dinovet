eb14364b9a329894bbe12ef264c471a74cb0591e
Chad Jablonski <chad@dinocore.net>
2011-01-26 15:33:44 -0800

    Style fixes for chrome, IE, and Opera

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,4 +23,33 @@ $(document).ready(function() {
   $('.tab-navigation ul li a').click(function(event) {
     event.stopPropagation()
   });
+
+  // Delete a phone number
+  $('fieldset.phone .delete').live('click', function(event) {
+    if(confirm("Are you sure you want to delete this phone number?")) {
+      $(this).parents('li.phone-number').remove();
+    }
+    return false;
+  });
+
+  // Add a phone number
+  $('fieldset.phone .add').click(function(event) {
+    $(this).parents('li').before('                                  \
+      <li id="phone-number-0" class="phone-number">                 \
+        <select name="client[phone_numbers][0][type]"               \
+            id="client_phone_numbers_0_type">                       \
+          <option selected="selected" value="Home">Home</option>    \
+          <option value="Cell">Cell</option>                        \
+        </select>                                                   \
+        <input type="text" value="" size="30"                       \
+            name="client[phone_numbers][0][number]"                 \
+            id="client_phone_numbers_0_number" autocomplete="off">  \
+        <a title="Delete this number" href="#" class="delete">      \
+          <img src="/images/delete.png?1295921241" alt="Delete">    \
+        </a>                                                        \
+      </li>                                                         \
+    ')
+  });
+
+  return false;
 });
